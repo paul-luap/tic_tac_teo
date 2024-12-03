78335fa0e4e8505a133bea755999bbe811dd9b71
paul luap <paul.luap@laplateforme.io>
2024-12-03 10:38:42 +0100

    grille de départ7

---
+++ tictactoe.py
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+
+
+
+#pour afficher la grille
+
+case_de_la_grille = [" "," "," "," "," "," "," "," "," "]
+
+def affich_grille():
+    print("\n")
+    print("-------------")
+    print("|",case_de_la_grille[0],"|",case_de_la_grille[1],"|",case_de_la_grille[2],"|")
+    print("-------------")
+    print("|",case_de_la_grille[3],"|",case_de_la_grille[4],"|",case_de_la_grille[5],"|")
+    print("-------------")
+    print("|",case_de_la_grille[6],"|",case_de_la_grille[7],"|",case_de_la_grille[8],"|")
+    print("-------------")
+    print("\n")
+
+affich_grille()
